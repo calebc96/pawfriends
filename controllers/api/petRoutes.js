e5ad8a992185f56b41c0d1c6f2beda581d552568
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-01-26 22:17:49 -0600

    single pet update

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -5,6 +5,7 @@ const { Pet } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const petData = await Pet.findAll();
+
     res.status(200).json(petData);
 
   } catch (err) {
@@ -13,12 +14,14 @@ router.get('/', async (req, res) => {
 });
 
 
+
 router.post('/', async (req, res) => {
   try {
     const petData = await Pet.create({
     name: req.body.name,
     location: req.body.location,
-    description: req.body.size,
+    description: req.body.description,
+    size:req.body.size,
     age: req.body.age,
     breed: req.body.breed,
     });
@@ -46,4 +49,29 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+router.get('/:id',async (req, res) => {
+  try {
+    const petData = await Pet.findByPk(req.params.id);
+
+    if (!petData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    const pet = petData.get({ plain: true });
+
+
+    // res.status(200).json(petData);
+    res.render('singlepet', { pet })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+
+
 module.exports = router;

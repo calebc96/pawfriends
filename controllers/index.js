e465caa388f195dd8a6cc13ca586b7adbf05ce92
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-26 21:51:28 -0500

    fixed server bugs

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+// router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")

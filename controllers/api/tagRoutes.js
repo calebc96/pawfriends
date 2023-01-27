02a4100f807c8a822423fefc81a66c8983534198
MitchH10 <mjh10@live.com>
2023-01-26 20:11:17 -0600

    added get route to tagRoutes.js

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { Tag } = require('../../models');
 
+router.get('/', async (req, res) => {
+    // find all tags
+    // be sure to include its associated Product data
+    try {
+      const tagData = await Tag.findAll();
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.post('/', async (req, res) => {
     try {
         const tagData = await Tag.create({

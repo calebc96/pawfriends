7ad224510ed64adb1a43e7f43a5cb3efa05ba126
kavic <kavicse.23@gmail.com>
2023-01-30 06:55:24 -0600

    Homepage rendered

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,27 @@
 const router = require('express').Router();
-
+const { Pet, User } = require('../models');
 module.exports = router;
+
+router.get('/', async (req, res) => {
+    try {
+        const petData = await Pet.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const pets = petData.map((pet) => pet.get({ plain: true}));
+   res.render('homepage', {
+    pets,
+    logged_in: req.session.logged_in
+   });
+   
+   
+   
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});

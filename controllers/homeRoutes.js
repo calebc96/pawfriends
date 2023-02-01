6a8cb769a603116898a2f60b474eb0fd61a6bc2f
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-31 20:16:13 -0500

    added a signup route if the user is already signed in they will be redirected to the homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,3 +25,14 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+  

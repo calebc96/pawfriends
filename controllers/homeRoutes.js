c4b58e8a17ee7d64c9be96f30fe3e5638ac3f43a
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-31 20:19:52 -0500

    added a homepage route that displays the pet data along with the user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,10 +29,34 @@ router.get('/', async (req, res) => {
 router.get('/signup', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/');
+      res.redirect('/homepage');
       return;
     }
   
     res.render('signup');
   });
   
+  router.get('/homepage', async (req, res) => {
+    try {
+      // Get all pets and JOIN with user data
+      const petData = await Pet.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const pets = petData.map((pet) => pet.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        pets, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });

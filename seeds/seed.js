0f5f536a61739955b25e1c2cb93eb9bf93eace80
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-02-01 20:17:28 -0500

    added seed data for adoptionform/picture.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,11 @@
-const sequelize = require('../config/connection');
-const { User, Pet, Tag } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Pet, Tag, Picture, Adoption } = require("../models");
 
-const userData = require('./userData.json');
-const petData = require('./petData.json');
-const tagData = require('./tagData.json');
+const userData = require("./userData.json");
+const petData = require("./petData.json");
+const tagData = require("./tagData.json");
+const pictureData = require("./picture.json");
+const adoptionData = require("./adoption.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -17,9 +19,13 @@ const seedDatabase = async () => {
   await Tag.bulkCreate(tagData);
   console.log(`${tagData.length} tags created`);
 
+  await Picture.bulkCreate(pictureData);
+  console.log(`${pictureData.length} pictures created`);
 
-process.exit(0);
+  await Adoption.bulkCreate(adoptionData);
+  console.log(`${adoptionData.length} adoption forms created`);
 
-}
+  process.exit(0);
+};
 
 seedDatabase();

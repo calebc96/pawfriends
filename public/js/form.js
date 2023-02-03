533a78e5eca40a54c770623fc6f71a9d222fc9b2
MitchH10 <mjh10@live.com>
2023-02-02 22:24:59 -0600

    fix form

--- public/js/form.js
+++ public/js/form.js
@@ -6,13 +6,13 @@ async function newFormHandler(event) {
     const size = document.querySelector('#pet_size').value;
     const age = document.querySelector('#pet_age').value;
     const breed = document.querySelector('#pet_breed').value;
-    let photo = document.getElementById("pet_image").files[0];
+    const photo = document.getElementById("pet_image").files[0];
     let formData = new FormData();
 
     const id = window.location.toString().split('/')[5];
 console.log(id);
     
-   
+   if (name && location && description && size && age && breed && photo) {
     const response = await fetch(`/api/pet`, {
       method: 'POST',
       body: JSON.stringify({
@@ -50,6 +50,46 @@ console.log(id);
     } else {
       alert('Missing pet picture');
     }
+   } else {
+    alert("Please include all info and a picture");
+   }
+    // const response = await fetch(`/api/pet`, {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //    name, 
+    //    location,
+    //    description, 
+    //    size, 
+    //    age, 
+    //    breed,
+
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
+    // const res = await response.json().then( data => data);
+    // const pet_id = res.pet_id;
+    // console.log(pet_id);
+    // if (response.ok) {
+    //   //document.location.replace('/petlist');
+    // } else {
+    //   alert('Missing pet information');
+    //   return
+    // }
+
+    // formData.append("photo", photo);
+    // formData.append("id", pet_id);
+    // const response2 = await fetch(`/api/pic`, {
+    //   method: 'POST',
+    //   body: formData,
+    // });
+    
+    // if (response2.ok) {
+    //   document.location.replace('/petlist');
+    // } else {
+    //   alert('Missing pet picture');
+    // }
   }
   
   document.querySelector('.new-pet-form').addEventListener('submit', newFormHandler);
